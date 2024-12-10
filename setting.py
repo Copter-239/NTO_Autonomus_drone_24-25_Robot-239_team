955fc7190119693e4242340fa6ff88043062bb61
Copter-239 <scherbinasu@gmail.com>
2024-12-10 14:47:07 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -2,7 +2,7 @@ import os
 from time import sleep
 open('/home/clover/catkin_ws/src/clover/clover/launch/aruco.launch','w').write(open('data/launch/aruco.launch','r').read())
 open('/home/clover/catkin_ws/src/clover/clover/launch/clover.launch','w').write(open('data/launch/clover.launch','r').read())
-os.system('mv data/model/* /home/clover/catkin_ws/src/clover/clover-simulation/models/')
+os.system('mv data/model/* /home/clover/catkin_ws/src/clover/clover_simulation/models/')
 
 
 
