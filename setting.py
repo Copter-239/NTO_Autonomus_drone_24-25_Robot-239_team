c3d6510dc83b7f8b2c680480592a3175b82848a1
Copter-239 <scherbinasu@gmail.com>
2024-12-10 14:42:03 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -3,7 +3,7 @@ from time import sleep
 open('/home/clover/catkin_ws/src/clover/clover/launch/aruco.launch','w').write(open('data/launch/aruco.launch','r').read())
 open('/home/clover/catkin_ws/src/clover/clover/launch/clover.launch','w').write(open('data/launch/clover.launch','r').read())
 os.system('mv data/model/* /home/clover/catkin_ws/src/clover/clover-simulation/models/')
-os.system("python3 world-random/rand_world.py")
+os.system("python3 world_random/rand_world.py")
 sleep(5)
 
 
